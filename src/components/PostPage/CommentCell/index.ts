59c5f3307d6610cc4c2f700682001905bf90b820
Jiaming Guo <64711081+j233guo@users.noreply.github.com>
2025-01-15 23:36:23 -0500

    supported displaying comment tree

--- src/components/PostPage/CommentCell/index.ts
+++ src/components/PostPage/CommentCell/index.ts
@@ -9,8 +9,11 @@ import { IUISetting, UIControlService } from "src/services/UIControlService";
 })
 export class CommentCell {
     @Input() comment: IComment
+    @Input() depth: number = 0
+    @Input() parentBorderColor?: string;
 
     uiSetting: IUISetting
+    borderColor: string
 
     constructor(
         private _uiControl: UIControlService,
@@ -19,7 +22,25 @@ export class CommentCell {
         effect(() => {
             this.uiSetting = this._uiControl.getUISetting()
         });
+        this.borderColor = this.getRandomColor()
     }
 
     ngOnInit() {}
+
+    private getRandomColor(): string {
+        const colors = [
+            '#FF4500',  // Reddit Orange
+            '#0079D3',  // Reddit Blue
+            '#46D160',  // Reddit Green
+            '#7193FF',  // Light Blue
+            '#FF4599',  // Pink
+            '#FFB000',  // Gold
+            '#9B84EC',  // Purple
+            '#FF6B33',  // Coral
+        ];
+        const availableColors = this.parentBorderColor ? 
+            colors.filter(color => color !== this.parentBorderColor) : 
+            colors;
+        return availableColors[Math.floor(Math.random() * availableColors.length)];
+    }
 }
